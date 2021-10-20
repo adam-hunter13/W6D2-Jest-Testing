edca474c1502c929d44779dff65107a49999e2ab
Adam Hunter <hunteradam13@gmail.com>
2021-10-20 09:49:37 -0600

    Adding more tests

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-
+let colors = ['Red', 'Green', 'Blue']
 
 module.exports = {
     returnTwo: (number) => {
@@ -7,6 +7,32 @@ module.exports = {
 
     greeting: (name) => {
         return `Hello, ${name}.`
+    },
+
+    color: (color) => {
+        return `My favorite color is ${color}.`
+    },
+
+    stateAge: (age) => {
+        return `I am ${age}.`
+    },
+
+    addUser: (name, age) => {
+        let user = {
+            name, 
+            age
+        }
+
+        if(age >= 21){
+            return user
+        } else {
+            return "User not old enough"
+        }
+    },
+
+    addColor: (color) => {
+        colors.push(color.toLowerCase())
+        return colors
     },
     
     add: (num1, num2) => {
@@ -27,5 +53,13 @@ module.exports = {
 
     square: (num1) => {
         return num1*num1
+    },
+
+    sqrt: (num1) => {
+        return Math.sqrt(num1)
+    },
+
+    negative: (num1) => {
+        return num1*-1
     }
 }
--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,6 @@
 // const { TestWatcher } = require('@jest/core')
 // const { TestWatcher } = require('@jest/core')
-const {returnTwo, greeting, add, multiply, divide, subtract, square} = require('./functions')
+const {returnTwo, greeting, color, stateAge, addUser, addColor, add, multiply, divide, subtract, square, sqrt, negative} = require('./functions')
 
 test('Should return 2', () => {
     expect(returnTwo(2)).toEqual('2')
@@ -8,6 +8,25 @@ test('Should return 2', () => {
 
 test('Should return Hello (name)', () => {
     expect(greeting('Adam')).toEqual('Hello, Adam.')
+})
+
+test('Should return My favorite color is (color).', () => {
+    expect(color('blue')).toEqual('My favorite color is blue.')
+})
+
+test('Should return I am [age]', ()=>{
+    expect(stateAge(25)).toEqual('I am 25.')
+})
+
+
+it('Should return user object or a failed status', () => {
+    expect(addUser('Joey', 21)).toEqual({name: 'Joey', age: 21})
+
+    expect(addUser('Chandler', 15)).toEqual('User not old enough')
+})
+
+it('Should add new color and return list of unique colors', () => {
+    expect(addColor('Pink')).toContain('pink')
 })
 
 describe('math functions', () => {
@@ -30,6 +49,14 @@ test('Should add num1 and num2', () => {
 
     test('Should square num1', () => {
         expect(square(5)).toBe(25)
+    })
+
+    test('Should provide the sqrt of num1', () => {
+        expect(sqrt(25)).toBe(5)
+    })
+
+    test('Should make the num1 negative', () => {
+        expect(negative(5)).toBe(-5)
     })
 
     })
